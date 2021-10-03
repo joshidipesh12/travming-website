d93e22ad33b0c20dde99209250cd186067471a82
Dipesh Joshi <joshidipesh12@gmail.com>
2021-10-03 15:47:48 +0530

    header and location overview setup

---
+++ components/HeaderBar.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+const HeaderBar = ({}) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.innerWidth > 750 ? false : true);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <Link href="#">TravMing</Link>
+      </h1>
+      <div className={styles.links}>
+        <h2 className={styles.link}>
+          <Link href="https://linkedin.com/in/joshidipesh12">About</Link>
+        </h2>
+        <h2 className={styles.link}>
+          <Link href="https://linkedin.com/in/joshidipesh12">Contact</Link>
+        </h2>
+      </div>
+      <div className={styles.links}>
+        <h2 className={styles.link} onClick={() => alert("Coming Soon!")}>
+          <Link href="#">Sign In</Link>
+        </h2>
+        <h2 className={styles.link} onClick={() => alert("Coming Soon!")}>
+          <Link href="#">Sign Up</Link>
+        </h2>
+        {!isMobile ? (
+          <Image
+            src={require("../icons/search.png")}
+            // className={styles.searchIcon}
+            id="search-icon"
+            height={20}
+            width={20}
+            layout="fixed"
+            onClick={() =>
+              alert(`i: ${window.innerWidth}  o: ${window.outerWidth}`)
+            }
+          />
+        ) : (
+          <Image
+            src={require("../icons/menu.svg")}
+            // className={styles.menuIcon}
+            id="menu-icon"
+            onClick={() => alert("Hello!")}
+          />
+        )}
+      </div>
+      <style jsx>
+        {`
+          @media (max-width: 750px) {
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default HeaderBar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import HeaderBar from "../components/Header";
+import { HeaderBar, FooterBar } from "../components";
+import config from "../dest-config.json";
+import { useState } from "react";
 
 export default function Home() {
+  const [country, setCountry] = useState("england");
+  const [province, setProvince] = useState("eastbourn");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,17 +17,28 @@ export default function Home() {
           name="description"
           content="TravMing - Across the World, Just a Few Clicks Away!"
         />
-        <meta name="theme-color" content="#77898e"/>
+        <meta name="theme-color" content="#77898e" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="manifest.json"/>
+        <link rel="manifest" href="manifest.json" />
       </Head>
       <header className={styles.header}>
         <HeaderBar />
       </header>
 
+      <div className={styles.background}>
+        <Image
+          src={`${config[country][province]}1920x1080`}
+          layout="fill"
+          placeholder="blur"
+          blurDataURL={`${config[country][province]}96x54`}
+        />
+      </div>
+
       <main className={styles.main}></main>
 
-      <footer className={styles.footer}></footer>
+      <footer className={styles.footer}>
+        <FooterBar country="england" />
+      </footer>
     </div>
   );
 }
