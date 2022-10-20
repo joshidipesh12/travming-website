8bc4fa0d1eaed7afbc21c9345df572ac37455aa7
Dipesh Joshi <joshidipesh12@gmail.com>
2022-10-20 17:15:33 +0530

    used normal axios

--- frontend/store/middleware/axios.js
+++ frontend/store/middleware/axios.js
@@ -1,28 +1,28 @@
-import localforage from 'localforage';
-import memoryDriver from 'localforage-memoryStorageDriver';
-import {setup} from 'axios-cache-adapter';
+// import localforage from 'localforage';
+// import memoryDriver from 'localforage-memoryStorageDriver';
+// import {setup} from 'axios-cache-adapter';
 
-async function configureAxios() {
-  if (!global.axios) {
-    await localforage.defineDriver(memoryDriver);
-    const forageStore = localforage.createInstance({
-      driver: [
-        localforage.INDEXEDDB,
-        localforage.LOCALSTORAGE,
-        memoryDriver._driver,
-      ],
-      name: 'axios-api-cache',
-    });
+// async function configureAxios() {
+//   if (!global.axios) {
+//     await localforage.defineDriver(memoryDriver);
+//     const forageStore = localforage.createInstance({
+//       driver: [
+//         localforage.INDEXEDDB,
+//         localforage.LOCALSTORAGE,
+//         memoryDriver._driver,
+//       ],
+//       name: 'axios-api-cache',
+//     });
 
-    global.axios = setup({
-      cache: {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
-        exclude: {methods: ['put', 'patch', 'delete']},
-        store: forageStore,
-      },
-    });
-  }
-  return global.axios;
-}
+//     global.axios = setup({
+//       cache: {
+//         maxAge: 1 * 24 * 60 * 60 * 1000,
+//         exclude: {methods: ['put', 'patch', 'delete']},
+//         store: forageStore,
+//       },
+//     });
+//   }
+//   return global.axios;
+// }
 
-export default configureAxios;
+// export default configureAxios;
